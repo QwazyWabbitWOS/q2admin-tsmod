2cd31c04d3aa5a5d0c226fa48c7bf5b926ec6aa7
QwazyWabbitWOS <gandalf.2004@gmail.com>
2024-01-21 10:20:42 -0800

    Stylistic change in Q_stricmp.

--- zb_util.c
+++ zb_util.c
@@ -40,16 +40,17 @@ void stuffcmd(edict_t *e, char *s)
  if s1 is contained within s2 then return 0, they are "equal".
  else return the lexicographic difference between them.
 */
-int	Q_stricmp(const char *s1, const char *s2)
+int Q_stricmp(const char* s1, const char* s2)
 {
-	const unsigned char
-		*uc1 = (const unsigned char *)s1,
-		*uc2 = (const unsigned char *)s2;
+	int result = 0;
+	const unsigned char* uc1 = (const unsigned char*)s1;
+	const unsigned char* uc2 = (const unsigned char*)s2;
 
-	while (tolower(*uc1) == tolower(*uc2++))
+	while ((result = Q_tolower(*uc1) - Q_tolower(*uc2++)) == 0)
 		if (*uc1++ == '\0')
-			return (0);
-	return (tolower(*uc1) - tolower(*--uc2));
+			break;
+
+	return result;
 }
 
 // required for the RE code
